a03e9ea3fd2df6fe24ee89921b238fbde1bbf615
Javierfloresr31 <49499366+Javierfloresr31@users.noreply.github.com>
2026-08-20 22:03:52 -0500

    feat(sprint-01): add idempotency conflict exception

---
+++ caso_de_uso_seguro/backend/sprint-01/src/main/java/pe/siniestrofacil/application/exception/IdempotencyConflictException.java
@@ -0,0 +1,7 @@
+package pe.siniestrofacil.application.exception;
+
+public class IdempotencyConflictException extends RuntimeException {
+    public IdempotencyConflictException(String message) {
+        super(message);
+    }
+}
